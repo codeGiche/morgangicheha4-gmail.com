a5d9f4712672e9104bddb45503dfd0cf4022756d
codeGiche <morgangicheha4@gmail.com>
2020-04-04 11:44:40 +0300

    working with travis ci

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ app.config["PROPAGATE_EXCEPTIONS"] = False
 # cofiguring db
 
 # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_RESTX")
-# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:morgan8514@127.0.0.1:5432/restx"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:morgan8514@127.0.0.1:5432/restx"
 app.config["SECRET_KEY"] = os.urandom(20)
 app.config["JWT_SECRET_KEY"] = os.urandom(20)
 # disable Try it Out for all methods
@@ -42,7 +42,7 @@ app.config["JWT_SECRET_KEY"] = os.urandom(20)
 
 ###########################TESTING CONFIG########################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db_test.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db_test.db"
 
 
 
